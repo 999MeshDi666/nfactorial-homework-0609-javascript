731d7ea07aa31e8af2555ebc14ddcebb3212aa85
999MeshDi666 <havkpzz@gmail.com>
2022-06-11 16:33:30 +0600

    theme mode

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
+$('.light').on('click', ()=>{
+    localStorage.setItem('color',"light");
+});
+$('.dark').on('click', ()=>{
+    localStorage.setItem('color',"dark");
+});
 
-let session_data = sessionStorage
+function getTheme(){
+    let theme = localStorage.getItem('color');
+    $('body').addClass(theme);
+    $('.main__btn').addClass(theme);
+}
+
+
+
+
+let session_data = sessionStorage;
 let userData;
-$('.registration-form').on("submit", ()=>{
+$('.registration-form').on("submit", (e)=>{
+    e.preventDefault();
     userData = $('#InputName').val() + " " + $('#InputSurname').val();
     session_data.setItem('user', userData);
     window.location.href = "./games.html";
-    $('.user-info').html(session_data.getItem('key'))
-    
+   
 });
 
 
+$('.user-info').html(`hi ${session_data.getItem('user')}`)
 $('.logout').on('click', ()=>{
     session_data.clear();
-
 })
+let tempExp = 'Wed, 15 Jun 2022 10:50:17 GMT';
+$('.set-coockie').on('click', ()=>{
+    document.cookie = `user = ${session_data.getItem('user')}; path = /; expires=${tempExp}`;
+})
+
